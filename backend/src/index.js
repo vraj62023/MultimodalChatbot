3da553d9d15270c12a77457b375942a40bbfe6b6
vraj62023 <vraj62023.24kgpian.iitkgp.ac.in>
2026-02-08 22:08:42 +0530

    containerised

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ const chatRoutes = require('./routes/chatRoutes');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: "http://localhost:5000/api/v1",
 });
 
 // 1. Attach Token to Request
