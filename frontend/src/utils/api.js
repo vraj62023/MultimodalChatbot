7611f5102053777aedcedf0e1fa53dba087c808f
vraj62023 <VRAJ62023.24@KGPIAN.IITKGP.AC.IN>
2026-04-17 02:03:46 +0530

    backend connect

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: import.meta.env.VITE_API_BASE_URL|| "http://localhost:5000/api/v1",
 });
 
 // 1. Attach Token to Request
